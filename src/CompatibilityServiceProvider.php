b5e666149a5a35b374e741b845280acd9098b52e
Jake Whiteley <odegramusic@gmail.com>
2019-10-27 14:17:09 +0000

    rename

--- src/CompatibilityServiceProvider.php
+++ src/CompatibilityServiceProvider.php
@@ -4,21 +4,19 @@ namespace Snap\Compatibility;
 
 use Snap\Services\Config;
 use Snap\Services\Container;
-use Snap\Services\Service_Provider;
+use Snap\Services\ServiceProvider;
 
 /**
  * Load various plugin compatibility fixes.
  */
-class Compatibility_Service_Provider extends Service_Provider
+class CompatibilityServiceProvider extends ServiceProvider
 {
     /**
      * Load any fixes.
-     *
-     * @since 1.0.0
      */
     public function boot()
     {
-        if ($this->is_offload_media_present() === true && Config::get('images.dynamic_image_sizes') !== false) {
+        if ($this->isOffloadMediaPresent() === true && Config::get('images.dynamic_image_sizes') !== false) {
             Container::resolve('\\Snap\\Compatibility\\Fixes\\Offload_Media')->run();
         }
     }
@@ -26,11 +24,9 @@ class Compatibility_Service_Provider extends Service_Provider
     /**
      * Check if WP Offload S3/Offload Media plugin active.
      *
-     * @since  1.0.0
-     *
      * @return boolean
      */
-    private function is_offload_media_present()
+    private function isOffloadMediaPresent()
     {
         if (isset($GLOBALS['as3cf'])
             || isset($GLOBALS['as3cfpro_compat_check'])
